a70b11a5bc4d9f7bd67fd039e6de97e50cdbfd32
chasin87 <yasinyuksek87@gmail.com>
2020-08-19 11:54:26 +0200

    add the copy of the images and styled memory

--- src/Components/Memory/index.js
+++ src/Components/Memory/index.js
@@ -4,44 +4,149 @@ import "./index.css";
 
 export default function Flip() {
   const animals = [
-    "Hond",
-    "Beer",
-    "Giraffe",
-    "Koala",
-    "Konijn",
-    "Leeuw",
-    "Nijlpaard",
-    "Olifant",
-    "Panda",
-    "Pinguen",
-    "Poes",
-    "Tijger",
-    "Vos",
-    "Wasbeer",
-    "Zebra",
+    {
+      name: "Beer",
+      id: 1,
+    },
+    {
+      name: "Beer",
+      id: 2,
+    },
+    {
+      name: "Giraffe",
+      id: 3,
+    },
+    {
+      name: "Giraffe",
+      id: 4,
+    },
+    { name: "Hond", id: 5 },
+    {
+      name: "Hond",
+      id: 6,
+    },
+    {
+      name: "Koala",
+      id: 7,
+    },
+    {
+      name: "Koala",
+      id: 8,
+    },
+    {
+      name: "Konijn",
+      id: 9,
+    },
+    {
+      name: "Konijn",
+      id: 10,
+    },
+    {
+      name: "Leeuw",
+      id: 11,
+    },
+    {
+      name: "Leeuw",
+      id: 12,
+    },
+    {
+      name: "Nijlpaard",
+      id: 13,
+    },
+    {
+      name: "Nijlpaard",
+      id: 14,
+    },
+    {
+      name: "Olifant",
+      id: 15,
+    },
+    {
+      name: "Olifant",
+      id: 16,
+    },
+    {
+      name: "Panda",
+      id: 17,
+    },
+    {
+      name: "Panda",
+      id: 18,
+    },
+    {
+      name: "Pinguen",
+      id: 19,
+    },
+    {
+      name: "Pinguen",
+      id: 20,
+    },
+    {
+      name: "Poes",
+      id: 21,
+    },
+    {
+      name: "Poes",
+      id: 22,
+    },
+    {
+      name: "Tijger",
+      id: 23,
+    },
+    {
+      name: "Tijger",
+      id: 24,
+    },
+    {
+      name: "Vos",
+      id: 25,
+    },
+    {
+      name: "Vos",
+      id: 26,
+    },
+    {
+      name: "Wasbeer",
+      id: 27,
+    },
+    {
+      name: "Wasbeer",
+      id: 28,
+    },
+    {
+      name: "Zebra",
+      id: 29,
+    },
+    {
+      name: "Zebra",
+      id: 30,
+    },
   ];
 
   return (
     <div className="container">
-      {animals.map((animal) => {
-        return (
-          <Flippy
-            flipOnClick={true} // default false
-            flipDirection="horizontal" // horizontal or vertical
-          >
-            <FrontSide className="Front"> </FrontSide>
-            <BackSide className="Back">
-              {" "}
-              <div>
-                <img
-                  className="image"
-                  src={require(`./images/${animal}.png`)}
-                />
-              </div>
-            </BackSide>
-          </Flippy>
-        );
-      })}
+      <div className="Cards">
+        <div className="Cards_In">
+          {animals.map((animal) => {
+            return (
+              <Flippy
+                flipOnClick={true} // default false
+                flipDirection="horizontal" // horizontal or vertical
+              >
+                <FrontSide className="Front"> </FrontSide>
+                <BackSide className="Back">
+                  {" "}
+                  <img
+                    className="image"
+                    src={require(`./images/${animal.name}.png`)}
+                    alt="animals"
+                  />{" "}
+                </BackSide>
+              </Flippy>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
