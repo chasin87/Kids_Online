128544903b68d94fae187153aee68fc4452eb141
chasin87 <yasinyuksek87@gmail.com>
2020-09-29 22:08:47 +0200

    Heroku settings correction

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,4 +1,2 @@
-export const apiUrl = "https://kids-online.herokuapp.com";
+export const apiUrl = process.env.API_URL || "http://localhost:5000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
-
-// process.env.API_URL || "http://localhost:5000";
