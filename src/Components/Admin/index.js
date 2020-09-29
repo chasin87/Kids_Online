4d1e29b9ede986cbda304b6fa8feaa4f3ab33268
chasin87 <yasinyuksek87@gmail.com>
2020-09-29 02:48:05 +0200

    test bug fix

--- src/Components/Admin/index.js
+++ src/Components/Admin/index.js
@@ -4,35 +4,35 @@ import { login } from "../../Store/user/actions";
 import { selectToken } from "../../Store/user/selectors";
 import { useHistory } from "react-router-dom";
 
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Box from "@material-ui/core/Box";
+// import Button from "@material-ui/core/Button";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import TextField from "@material-ui/core/TextField";
+// import Box from "@material-ui/core/Box";
 
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
+// import Typography from "@material-ui/core/Typography";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Container from "@material-ui/core/Container";
 import "./index.css";
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    background: "Tomato !important",
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   paper: {
+//     marginTop: theme.spacing(8),
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//   },
+//   form: {
+//     width: "100%",
+//     marginTop: theme.spacing(1),
+//   },
+//   submit: {
+//     margin: theme.spacing(3, 0, 2),
+//     background: "Tomato !important",
+//   },
+// }));
 
 export default function Admin() {
-  const classes = useStyles();
+  // const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
@@ -52,58 +52,113 @@ export default function Admin() {
 
     setEmail("");
     setPassword("");
-    console.log("token", token);
-    console.log("history", history);
   }
 
   return (
-    <Container component="main" maxWidth="xs">
-      <div className="title_admin">Admin Login </div>
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5"></Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            // autoComplete="email"
-            autoFocus
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type="email"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            // autoComplete="current-password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            onClick={submitForm}
-          >
-            Sign In
-          </Button>
+    <div className="Container">
+      <div class="login-form">
+        <form>
+          <div className="title_admin">Admin Login </div>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"></span>
+              </div>
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Email"
+                required="required"
+                id="email"
+                name="email"
+                // autoComplete="email"
+                // autoFocus
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <span class="input-group-text"></span>
+              </div>
+              <input
+                type="password"
+                class="form-control"
+                placeholder="Password"
+                required="required"
+                label="Password"
+                id="password"
+                // autoComplete="current-password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <button
+              type="submit"
+              class="btn btn-primary btn-block"
+              fullWidth
+              variant="contained"
+              onClick={submitForm}
+            >
+              Log in
+            </button>
+          </div>
         </form>
       </div>
-      <Box mt={8}></Box>
-    </Container>
+    </div>
   );
 }
+
+// <Container component="main" maxWidth="xs">
+// <div className="title_admin">Admin Login </div>
+// <CssBaseline />
+
+// <div className={classes.paper}>
+//   <form className={classes.form} noValidate>
+//     <TextField
+//       variant="outlined"
+//       margin="normal"
+//       required
+//       fullWidth
+//       id="email"
+//       label="Email Address"
+//       name="email"
+//       // autoComplete="email"
+//       // autoFocus
+//       value={email}
+//       onChange={(event) => setEmail(event.target.value)}
+//       type="email"
+//     />
+//     <TextField
+//       variant="outlined"
+//       margin="normal"
+//       required
+//       fullWidth
+//       name="password"
+//       label="Password"
+//       type="password"
+//       id="password"
+//       // autoComplete="current-password"
+//       value={password}
+//       onChange={(event) => setPassword(event.target.value)}
+//     />
+
+//     <Button
+//       type="submit"
+//       fullWidth
+//       variant="contained"
+//       color="primary"
+//       className={classes.submit}
+//       onClick={submitForm}
+//     >
+//       Sign In
+//     </Button>
+//   </form>
+// </div>
+
+// <Box mt={8}></Box>
+// </Container>
