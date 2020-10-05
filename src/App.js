d673e6873104210bc924fd3d77e9ed483afe9ce1
chasin87 <yasinyuksek87@gmail.com>
2020-10-05 17:50:36 +0200

    messagebox and loading added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Switch, Route } from "react-router-dom";
+import { selectAppLoading } from "./Store/appState/selectors";
 import "./App.css";
 import Navigation from "./Components/Navigation";
 import Home from "./pages/Home/";
@@ -9,12 +11,18 @@ import Memory from "./pages/Memory/";
 import QuizFunc from "./pages/Quiz/";
 import AdminLog from "./pages/Admin/";
 import QuizDash from "./pages/QuizDashboard/";
+import MessageBox from "./Components/MessageBox";
+import Loading from "./Components/Loading";
 
 function App() {
+  const isLoading = useSelector(selectAppLoading);
+
   return (
     <div className="App">
       <Navigation />
+      <MessageBox />
       <Switch>
+        {isLoading ? <Loading /> : null}
         <Route exact path="/" component={Home} />;
         <Route path="/kleuren" component={Kleuren} />;
         <Route path="/tictactoe" component={TicTacToe} />;
--- src/Store/rootReducer.js
+++ src/Store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import user from "./user/reducer";
+import appState from "./appState/reducer";
 
 export default combineReducers({
   user,
+  appState,
 });
