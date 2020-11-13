ed4200b6e0d6ae1a5e663014060f2abf0b16169f
chasin87 <yasinyuksek87@gmail.com>
2020-11-13 16:26:15 +0100

    get answers by question id

--- src/Components/QuizDashboard/Questions_index.js
+++ src/Components/QuizDashboard/Questions_index.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Question_index.css";
-import { Card, Accordion, Button } from "react-bootstrap";
+import { Card, Accordion, Button, Modal } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { selectquizzes } from "../../Store/quizlist/selectors";
 import { fetchQuizList } from "../../Store/quizlist/actions";
@@ -9,6 +9,7 @@ import { fetchAnswerList } from "../../Store/answerlist/actions";
 import { Link } from "react-router-dom";
 import { selectUser } from "../../Store/user/selectors";
 import { useHistory } from "react-router-dom";
+
 import Axios from "axios";
 import AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
@@ -16,6 +17,10 @@ import "react-h5-audio-player/lib/styles.css";
 // import { questions } from "../Quiz";
 
 export default function QuizQuestions() {
+  const [show, setShow] = useState(false);
+  const [ids, setIds] = useState();
+  const [question, setQuestion] = useState("");
+
   const { token } = useSelector(selectUser);
   const history = useHistory();
   if (token === null) {
@@ -31,6 +36,16 @@ export default function QuizQuestions() {
       console.log(res.data);
       dispatch(fetchQuizList());
     });
+  };
+
+  const showAnswers = (id) => {
+    console.log(id);
+    setShow(true);
+  };
+
+  const handleClose = () => {
+    setShow(false);
+    setIds();
   };
 
   useEffect(() => {
@@ -56,7 +71,7 @@ export default function QuizQuestions() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M10 12.796L4.519 8 10 3.204v9.592zm-.659.753l-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"
                 />
               </svg>
@@ -75,7 +90,7 @@ export default function QuizQuestions() {
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M6 12.796L11.481 8 6 3.204v9.592zm.659.753l5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"
                 />
               </svg>
@@ -84,10 +99,11 @@ export default function QuizQuestions() {
         </div>
 
         <div className="container_title">Quiz Questions</div>
+
         <div>
           {Quizzes.map((quest) => {
             return (
-              <div className="question_part">
+              <div className="question_part" key={quest.id}>
                 <Accordion>
                   <Card>
                     <Accordion.Toggle as={Card.Header} eventKey="0">
@@ -110,16 +126,15 @@ export default function QuizQuestions() {
                           <div className="iconn">
                             <div className="iconArrow">
                               <svg
-                                className="iconArr"
+                                className="iconArr bi bi-arrow-down-square-fill"
                                 width="40px"
                                 height="40px"
                                 viewBox="0 0 16 16"
-                                class="bi bi-arrow-down-square-fill"
                                 fill="tomato"
                                 xmlns="http://www.w3.org/2000/svg"
                               >
                                 <path
-                                  fill-rule="evenodd"
+                                  fillRule="evenodd"
                                   d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
                                 />
                               </svg>
@@ -130,7 +145,7 @@ export default function QuizQuestions() {
                                 width="40px"
                                 height="40px"
                                 viewBox="0 0 16 16"
-                                class="bi bi-x"
+                                className="bi bi-x"
                                 fill="currentColor"
                                 xmlns="http://www.w3.org/2000/svg"
                                 onClick={(e) => {
@@ -138,7 +153,7 @@ export default function QuizQuestions() {
                                 }}
                               >
                                 <path
-                                  fill-rule="evenodd"
+                                  fillRule="evenodd"
                                   d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
                                 />
                               </svg>
@@ -161,6 +176,7 @@ export default function QuizQuestions() {
                               {quest.question}
                             </div>
                           </div>
+
                           <AudioPlayer
                             header="Question Sound"
                             src={quest.questionSound}
@@ -171,7 +187,17 @@ export default function QuizQuestions() {
                             customVolumeControls={[]}
                           />
                           <div className="question_footer">
-                            <Button className="answer_button">Answers</Button>
+                            <Button
+                              className="answer_button"
+                              // onClick={showAnswers}
+                              onClick={(id, question) => {
+                                showAnswers(quest.id);
+                                setIds(quest.id);
+                                setQuestion(quest.question);
+                              }}
+                            >
+                              Answers
+                            </Button>
                           </div>
                         </div>
 
@@ -180,12 +206,12 @@ export default function QuizQuestions() {
                             width="40px"
                             height="40px"
                             viewBox="0 0 16 16"
-                            class="bi bi-pencil"
+                            className="bi bi-pencil"
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
                             />
                           </svg>
@@ -194,7 +220,7 @@ export default function QuizQuestions() {
                             width="40px"
                             height="40px"
                             viewBox="0 0 16 16"
-                            class="bi bi-x"
+                            className="bi bi-x"
                             fill="currentColor"
                             xmlns="http://www.w3.org/2000/svg"
                             onClick={(e) => {
@@ -202,32 +228,51 @@ export default function QuizQuestions() {
                             }}
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"
                             />
                           </svg>
                         </div> */}
-                        {Answers.map((answer) => {
-                          if (quest.id === answer.quizId) {
-                            return (
-                              <div>
-                                <p>{answer.answer}</p>
-                                <img
-                                  className="question_image_format question_info"
-                                  src={answer.answerImage}
-                                  alt="answerImage"
-                                />
-                                <audio controls>
-                                  <source src={answer.answerSound}></source>
-                                </audio>
-                                <p>{answer.quizId}</p>
-                                <p>{answer.isCorrect.toString()}</p>
-                              </div>
-                            );
-                          } else {
-                            return console.log("nooo");
-                          }
-                        })}
+
+                        <Modal centered show={show} onHide={handleClose}>
+                          <Modal.Header className="header_modal">
+                            <Modal.Title>
+                              Answers for
+                              <br />
+                              {question}
+                            </Modal.Title>
+                          </Modal.Header>
+
+                          {Answers.map((answer) => {
+                            if (ids === answer.quizId) {
+                              return (
+                                <Modal.Body>
+                                  <div key={answer.id}>
+                                    <p>Answer in text:{answer.answer}</p>
+                                    <img
+                                      className="question_image_format question_info"
+                                      src={answer.answerImage}
+                                      alt="answerImage"
+                                    />
+                                    <audio controls>
+                                      <source src={answer.answerSound}></source>
+                                    </audio>
+                                    <p>{answer.quizId}</p>
+                                    <p>{answer.isCorrect.toString()}</p>
+                                  </div>
+                                </Modal.Body>
+                              );
+                            } else {
+                              return null;
+                            }
+                          })}
+
+                          <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>
+                              Close
+                            </Button>
+                          </Modal.Footer>
+                        </Modal>
                       </Card.Body>
                     </Accordion.Collapse>
                   </Card>
