46914b8ad7e53786d7d3df28cd105881f9b98624
chasin87 <yasinyuksek87@gmail.com>
2020-11-14 01:28:46 +0100

    deploying to heroku for test

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
-  process.env.API_URL || "http://localhost:8888";
+  process.env.API_URL || "https://kids-online.herokuapp.com";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
 
-// "https://kids-online.herokuapp.com"
+// "http://localhost:8888";
