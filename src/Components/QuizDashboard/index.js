8625766733fa5c1b18445d563cf40ac373c9bd5d
chasin87 <yasinyuksek87@gmail.com>
2020-12-12 15:12:57 +0100

    fixed bug fix removed import useState

--- src/Components/QuizDashboard/index.js
+++ src/Components/QuizDashboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./index.css";
 import { fetchAnswerQuantity } from "../../Store/quantity/actions";
 import { useSelector, useDispatch } from "react-redux";
